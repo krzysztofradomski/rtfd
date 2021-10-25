a07d7ba7457f6327aabd158ad3d5fc93894d7567
Krzysztof Radomski <krzysztof@radomski.io>
2021-10-25 17:47:41 +0200

    fixes to style and content

--- src/components/EmployeeManagement.js
+++ src/components/EmployeeManagement.js
@@ -2,27 +2,31 @@ import { useState } from "react"
 
 const EmployeeForm = ({onSubmit = () => {}, disabled = true }) => (
     <form onSubmit={onSubmit}>
-        <input type="text" name="name" placeholder="Name" required/>
-        <input type="text" name="surname" placeholder="Surname" required />
-        <button type="submit" disabled={disabled}>Submit</button>
+        <input type="text" name="name" placeholder="Imię" required/>
+        <input type="text" name="surname" placeholder="Nazwisko" required />
+        <button type="submit" disabled={disabled}>Dodaj</button>
     </form>
 )
 
-const EmployeeList = ({employees = [], removeEmployee}) => (
+const EmployeeList = ({employees = [], removeEmployee  = () => {}}) => (
     <table>
-        <tr>
-            <th>Name</th>
-            <th>Surname</th>
-            <th>Remove</th>
-        </tr>
-       {console.log(employees, removeEmployee)}
-        {employees.map(({id, name, surname}) => (
-            <tr key={id}>
-                <td >{name}</td>
-                <td>{surname}</td>
-                <td><button onClick={() => removeEmployee(id)}>Usuń</button></td>
+        <thead>
+            <tr><td colSpan="3">Pracownicy</td></tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th>Imię</th>
+                <th>Nazwisko</th>
+                <th>Usuń</th>
             </tr>
-        ))}
+            {employees.map(({id, name, surname}) => (
+                <tr key={id}>
+                    <td >{name}</td>
+                    <td>{surname}</td>
+                    <td><button onClick={() => removeEmployee(id)}>Usuń</button></td>
+                </tr>
+            ))}
+        </tbody>
     </table>
 )
 
@@ -37,6 +41,7 @@ const EmployeeManagement = () => {
             surname: formData.get("surname")
         }
         setEmployees([...employees, newEmployee])
+        event.target.reset()
     }
 
     const removeEmployee = (id) => setEmployees(employees.filter(employee => employee.id !== id))
